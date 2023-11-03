42597c04b5e4725e7967080f80eff9937ad3b091
ekiidx <info@vuedesign.co>
2023-11-02 22:24:28 -0400

    updated header

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   
 <head>
@@ -6,14 +6,52 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Vibe | Music and Event Website | Vue Design</title>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link href="/assets/css/main.css" rel="stylesheet">
 </head>
 
 <body>
 
 <header>
-    <nav class="navbar navbar-expand-md navbar-dark absolute-top">
+
+
+<nav class="navbar navbar-dark navbar-expand-lg" data-bs-theme="dark">
+  <div class="container">
+
+    <a class="navbar-brand" href="/">BPM</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a></li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul></li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a></li>
+      </ul>
+      
+      <!-- <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
+    </div>
+  </div>
+</nav>
+
+    <!-- <nav class="navbar navbar-expand-md navbar-dark absolute-top">
       <div class="container">
         <a class="navbar-brand" href="index.php">Vibe</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,9 +81,8 @@
           </li>
         </ul>
       </div>
-    </nav>
+    </nav> -->
 </header>
-
 
 <section id="hero-main" class="hero-main">
   <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
